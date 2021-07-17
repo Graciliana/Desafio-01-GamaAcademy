54e84bfc6fa2b9dec3bc7736f37b55a16c7c7c25
Graciliana <eagraciliana@gmail.com>
2021-07-17 13:14:06 -0300

    Atualizado o site com o javascript

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
+let emailInput = document.querySelector('.email-input')
 
+function newCustomer(){
+    let email = emailInput.value
+    validateEmail(email)
+
+    if(validateEmail(email) === false){
+        return document.querySelector('.email-input').value = "" ,alert('Coloque um E-mail valido como : augusto@gmail.com')
+    } else{
+        localStorage.setItem("userEmail", JSON.stringify(email))
+        return document.querySelector('.email-input').value = "", alert('E-mail salvo com sucesso!!')
+    }
+    
+}
+
+function validateEmail(email) {
+        var re = /\S+@\S+\.\S+/;
+        return re.test(email);
+ }
+    
